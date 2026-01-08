2d6811d9bb9ee2e55ed50bcce17b9fee0298a728
cf987 <cf9874@gmail.com>
2026-01-09 03:01:51 +0900

    lec 3 . box design

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Contact App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      // home: Text('Contact App'),
-      // home: Icon(Icons.star),
-      // home: Image.asset('alp.jpg'),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Contact App', style: TextStyle(color: Colors.white)),
           backgroundColor: Colors.blue,
         ),
-        body: Row(
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [Icon(Icons.star), Icon(Icons.star_border)],
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
+        body: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
             width: double.infinity,
-            height: 50,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
+            height: 150,
+            // color: Colors.red,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width: 2),
+              borderRadius: BorderRadius.circular(10),
             ),
+            child: Text('Hello'),
           ),
         ),
       ),
