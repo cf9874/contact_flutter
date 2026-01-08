838cb6453597cb07fa041a9c3939167bb5d100f3
cf987 <cf9874@gmail.com>
2026-01-09 02:46:04 +0900

    lec2 ; basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,25 @@ class MyApp extends StatelessWidget {
       // home: Text('Contact App'),
       // home: Icon(Icons.star),
       // home: Image.asset('alp.jpg'),
-      home: Center(
-        child: Container(
-          color: Colors.red,
-          width: 150,
-          height: 150,
-          child: Text('Hello'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Contact App', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blue,
+        ),
+        body: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [Icon(Icons.star), Icon(Icons.star_border)],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page),
+            ],
+          ),
         ),
       ),
     );
